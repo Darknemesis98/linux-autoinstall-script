6dd2e98bdeebce32c174d97c99ff35893e2a0cec
Hrishikesh G <48472158+Darknemesis98@users.noreply.github.com>
2019-10-20 18:02:48 +0530

    Update appshell.sh

--- appshell.sh
+++ appshell.sh
@@ -1,45 +1,44 @@
-##!/bin/bash
-echo Hi $USER
-hostnamectl
-echo 
-echo
+#!/bin/bash
 
 manjaroupdate()
 {
-	echo "update? <Y / n>"
-	read reply
-
-	if [ $reply == "" ] 
-		then
-			echo Update process initiated.
-			sudo pacman-mirrors -f 20
-			sudo pacman -Syu
-	else
-		echo ok, update cancelled
-	fi
+echo
+echo Update process initiated.
+echo Manjaro updating..
+echo
+sudo pacman-mirrors -f 20
+echo
+sudo pacman -Syu
 }
 
 debianupdate()
 {
-	echo "update? <Y / n>"
-	read reply
+echo
+echo Update process initiated.
+echo debian updating...
+echo
+sudo apt update && sudo apt upgrade
+}
 
-	if [ $reply == "" ] 
-		then
-			echo Update process initiated.
-			sudo apt update && sudo apt upgrade
-	else
-		echo ok, update cancelled
-	fi
+cancel()
+{
+echo
+echo Process successfully terinated.
+echo
 }
 
 ugetint()
 {
-	echo installing uget integrator
-	wget https://raw.githubusercontent.com/ugetdm/uget-integrator/master/install/linux/install_uget_integrator.sh
-	chmod +x install_uget_integrator.sh
-	./install_uget_integrator.sh
-	rm install_uget_integrator.sh 
+echo
+echo installing uget integrator
+echo
+wget https://raw.githubusercontent.com/ugetdm/uget-integrator/master/install/linux/install_uget_integrator.sh
+chmod +x install_uget_integrator.sh
+./install_uget_integrator.sh
+rm install_uget_integrator.sh 
+echo
+echo uget intergrated
+echo
 }
 
 removegt()
@@ -49,69 +48,85 @@ sudo pacman -R gnome-terminal
 
 manjaroinstall()
 {
-	manjaroupdate
-
-	echo installing apps:
-	sudo pacman -S tilix pacaur uget snapd nano unzip unrar dolphin yay git screenfetch conky conky-manager vlc albert code cmake libreoffice-still cmake htop
-
-	ugetint
-	removegt
-
+echo installing apps:
+echo
+sudo pacman -S tilix pacaur uget snapd nano unzip unrar dolphin yay git screenfetch conky conky-manager vlc albert code cmake libreoffice-still cmake htop
+echo
+ugetint
+removegt
+echo
 }
 
 
 debianinstall()
 {
-
-	sudo apt-get install tilix uget snapd nano unzip unrar dolphin git screenfetch conky conky-manager vlc cmake libreoffice-still cmake htop ssh
-
-	ugetint
-	removegt
-	
+echo installing apps:
+echo
+sudo apt-get install tilix uget snapd nano unzip unrar dolphin git screenfetch conky conky-manager vlc cmake libreoffice-still cmake htop ssh
+echo
+ugetint
+removegt
+echo
 }
-
 
 bashappend()
 {
-	echo >> $HOME/.bashrc
-	echo >> $HOME/.bashrc
-	echo >> $HOME/.bashrc
-	echo "#my lines below" >> $HOME/.bashrc
-	echo "PS1='\[\033[1;36m\]\u\[\033[1;31m\]@\[\033[1;32m\]\h:\[\033[1;35m\]\w\[\033[1;31m\]\$\[\033[0m\] '" >> $HOME/.bashrc
-	echo >> $HOME/.bashrc
-	echo "screenfetch" >> $HOME/.bashrc
-	echo Done!
+echo >> $HOME/.bashrc
+echo >> $HOME/.bashrc
+echo >> $HOME/.bashrc
+echo "#my lines below" >> $HOME/.bashrc
+echo "PS1='\[\033[1;36m\]\u\[\033[1;31m\]@\[\033[1;32m\]\h:\[\033[1;35m\]\w\[\033[1;31m\]\$\[\033[0m\] '" >> $HOME/.bashrc
+echo >> $HOME/.bashrc
+echo "screenfetch" >> $HOME/.bashrc
+echo Done!
 }
 
-echo "Distro? <MANJARO / debian>"
+installapps()
+{
+if [ $DISTRO -eq 1 ]
+	then manjaroinstall
+else debianinstall
+fi 
+}
 
-read DISTRO
+updatedistro()
+{
+if [ $DISTRO -eq 1 ]
+	then manjaroupdate
+elif [ $DISTRO -eq 0 ]
+	then debianupdate
+fi 
+}
 
-if [ $DISTRO == ""] 
-	then DISTRO="manjaro"
+#MAIN
+
+echo Hi $USER,
+echo "System info:"
+hostnamectl
+echo 
+echo "1 for manjaro"
+echo "0 for debian/ubuntu"
+
+echo "Distro? <manjaro or debian>"
+read  DISTRO
+if [ "$DISTRO" == "" ] 
+	then DISTRO="1"
 fi
 
-case "$DISTRO" in 
-    #case 1 
-    "manjaro") manjaroinstall ;; 
-      
-    #case 2 
-    "debian") echo "H" ;; 
-esac 
+echo "Update? <1/0>"
+read UPDATE
 
-
-echo "Add bashrc edits? <Y/n>"
-
-read NEW
-
-if [ $NEW == ""] 
-	then NEW="yes"
+if [ $UPDATE -eq 1 ]
+then updatedistro
+else cancel
 fi
 
-case "$NEW" in 
-    #case 1 
-    "yes") bashappend ;; 
-      
-    #case 2 
-    "n") echo Ok Done! ;; 
-esac
+installapps
+
+echo "Add bashrc edits? <1/0>"
+read  NEW
+
+if [ $NEW -eq 1 ]
+then bashappend
+else cancel
+fi
