1a38e23c9cc85895d2ac2c5eec2fe76564c2052b
Darknemesis98 <hrishikesh.gopakumar@gmail.com>
2020-02-05 23:35:26 +0530

    gitinstall final version

--- gitinstall.sh
+++ gitinstall.sh
@@ -1,14 +1,19 @@
 #! /bin/bash
+echo "created by Darknemesis98"
+echo "GitHub Configuration:"
+read -p "Enter your GitHub username: " username
+read -p "Enter email id: " email
 
-input $username
-input $email
-
+echo "updating parameters"
 git config --global user.name "$username"
 git config --global user.email "$email"
 git config --global color.ui true
 git config --global core.editor emacs
-$ ssh-keygen -t rsa -b 4096 -C "$email"
+
+echo "Generating ssh-key"
+ssh-keygen -t rsa -b 4096 -C "$email"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
+
 xclip -sel clip < ~/.ssh/id_rsa.pub
 echo 'copied ssh-key'
